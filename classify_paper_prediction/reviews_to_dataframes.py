a73decce37784ac8358360d748032b3fe2b6b1f0
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2021-04-08 03:30:40 -0700

    Added accepted field in reviews dataframe

--- classify_paper_prediction/reviews_to_dataframes.py
+++ classify_paper_prediction/reviews_to_dataframes.py
@@ -6,17 +6,20 @@ import json
 
 def read_reviews_from_json(path_to_json):
     try:
-        df = pd.read_json(open(path_to_json, "r", encoding="utf8"))
+        df = pd.read_json(open(path_to_json))
     except:
         # df = pd.read_json(open(path_to_json), orient='table')
         with open(path_to_json) as json_data:
             data = json.load(json_data)
-            df = pd.DataFrame({"id": data["id"], "title": data["title"], "abstract": data["abstract"], "reviews": data["reviews"]})
+            if "accepted" in data:
+                df = pd.DataFrame({"id": [data['id']], "title": [data['title']], "abstract": [data['abstract']], "reviews": [data['reviews']], "accepted": [data['accepted']]})
+            else:
+                df = pd.DataFrame({"id": [data['id']], "title": [data['title']], "abstract": [data['abstract']], "reviews": [data['reviews']]})
     return df
 
 def create_reviews_dataframe():
     rootdir = '/home/kapil/SJSU-Acad/MastersProject/codebase/data'
-    columns_list = ["id", "title", "abstract", "reviews"]
+    columns_list = ["id", "title", "abstract", "reviews", "accepted"]
     df = pd.DataFrame(columns=columns_list)
     count = 0
     valueErrorCount = 0
@@ -26,7 +29,7 @@ def create_reviews_dataframe():
             for x, y, filenames in os.walk(subdir):
                 for filename in filenames:
                     try:
-                        df1 = pd.DataFrame(read_reviews_from_json(subdir + '/' + filename))
+                        df1 = read_reviews_from_json(subdir + '/' + filename)
                         df = df.append(df1[df1.columns & columns_list], ignore_index=True)
                         # df = df.append(df1)
                         count += 1
@@ -44,4 +47,8 @@ def create_reviews_dataframe():
 if __name__ == '__main__':
     df = create_reviews_dataframe()
     print(len(pd.unique(df['id'])))
+    print(len(pd.unique(df['title'])))
+    print(len(pd.unique(df['abstract'])))
+    print(df.count())
+    # print(len(pd.unique(df['id'])))
     print()
